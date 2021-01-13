d8e445171952c6c0d79bd84b1d8e5835d55ba435
sean <maherseanmilo@gmail.com>
2021-01-13 16:08:57 -0600

    adds route/view for single product display

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,13 @@
 class Api::ProductsController < ApplicationController
+
   def show_all_products_action
     @all_products = Product.all
     render 'show_all_products.json.jb'
   end
+
+  def show_single_product_action
+    @single_product = Product.first
+    render 'show_single_product.json.jb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get '/all_products_path' => 'products#show_all_products_action'
+    get 'single_product_path' => 'products#show_single_product_action'
   end
 end
