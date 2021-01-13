403d9420d92201111b324b089b116ad711690ed9
sean <maherseanmilo@gmail.com>
2021-01-13 14:20:55 -0600

    adds 5 Product instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(
+  [
+    {
+      name: "Judge",
+      price: 12,
+      description: "Stable putter with a beaded rim and a flat profile."
+    },
+    {
+      name: "Deputy",
+      price: 12,
+      description: "Understable putter with a beaded rim and a flat profile."
+    },
+    {
+      name: "Truth",
+      price: 15,
+      description: "Neutral midrange with a beaded rim and a flat to slightly domey profile."
+    },
+    {
+      name: "Verdict",
+      price: 15,
+      description: "Overstable midrange with a beaded rim and a flat to slightly domey profile."
+    },
+    {
+      name: "Getaway",
+      price: 15,
+      description: "Stable fairway driver with a medium-width rim and a domey profile."
+    }
+  ]
+)
