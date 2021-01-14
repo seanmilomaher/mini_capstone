ceeaff0b957300d849bc0d87740319b3070021b3
sean <maherseanmilo@gmail.com>
2021-01-14 11:46:16 -0600

    updates single product view

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,12 +1,12 @@
 class Api::ProductsController < ApplicationController
 
   def show_all_products_action
-    @all_products = Product.all
+    @products = Product.all
     render 'show_all_products.json.jb'
   end
 
   def show_single_product_action
-    @single_product = Product.first
+    @product = Product.first
     render 'show_single_product.json.jb'
   end
 
