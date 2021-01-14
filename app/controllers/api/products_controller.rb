b509d0e524007c90b230dfee1948f4e3589bb744
sean <maherseanmilo@gmail.com>
2021-01-14 12:45:39 -0600

    adds second product view and updates previous views

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,13 +1,17 @@
 class Api::ProductsController < ApplicationController
 
-  def show_all_products_action
+  def all_products_action
     @products = Product.all
-    render 'show_all_products.json.jb'
+    render 'all_products.json.jb'
   end
 
-  def show_single_product_action
+  def first_product_action
     @product = Product.first
-    render 'show_single_product.json.jb'
+    render 'first_product.json.jb'
+  end
+  def second_product_action
+    @product = Product.second
+    render 'second_product.json.jb'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get '/all_products_path' => 'products#show_all_products_action'
-    get 'single_product_path' => 'products#show_single_product_action'
+    get '/all_products_path' => 'products#all_products_action'
+    get 'first_product_path' => 'products#first_product_action'
+    get 'second_product_path' => 'products#second_product_action'
   end
 end
